05a1772cac0012e2dfc0d8edebfd918f81f5217a
Josh the cat <catterjeeo@gmail.com>
2024-01-12 21:55:53 +0530

    Python/Nonsens/WordGen.py was changed to give multiple outputs.

--- Python/Nonsens/WordGen.py
+++ Python/Nonsens/WordGen.py
@@ -13,7 +13,7 @@ def normalize(dis):
         dis[i] = dis[i] / su
 
 def weigrand(dis):
-    l = random.random()
+    l = random.random();
     su = 0;
     for i in dis:
         l += -1 * dis[i];
@@ -22,16 +22,18 @@ def weigrand(dis):
     if su == 0: return None;
     return 'z'
 
-for i in weights:
-    normalize(weights[i]);
+def main(seed = "A", leng = 3):
+    out = seed;
+    for _ in range(int(leng)):
+        di = weights[out[-1]];
+        chis = weigrand(di);
+        if not chis: break;
+        out += chis;
+    print(out);
 
-seed = input("Seed? ") or random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-leng = input("Length? ") or round(random.random() * 17);
-out = seed;
-for _ in range(int(leng)):
-    di = weights[out[-1]];
-    chis = weigrand(di);
-    if not chis: break;
-    out += chis;
-
-print(out);
+if __name__ == "__main__":
+    for i in weights:
+        normalize(weights[i]);
+    seed = input("Seed? ") or random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    leng = input("Length? ") or round(random.random() * 17);
+    while input() !="q": main(seed, leng)
