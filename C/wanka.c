65a192f5b8e5ad43d63ade7e71636c59585056cc
Josh the cat <catterjeeo@gmail.com>
2024-01-15 17:21:30 +0530

    "wanka.c was fixed to use scanf_s"

--- C/wanka.c
+++ C/wanka.c
@@ -1,33 +1,33 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define fill(...) fill_handle((f_args){__VA_ARGS__})
-typedef struct{
-char* strin;
-char fillchar;
-int len;
-}f_args;
-char* fill_base(char* b, char a, int len){
-for(int i=0;i<len;i++){
-    *(b+i)=a;
+typedef struct {
+  char *strin;
+  char fillchar;
+  int len;
+} f_args;
+char *fill_base(char *b, char a, int len) {
+  for (int i = 0; i < len; i++) {
+    *(b + i) = a;
+  }
+  return b;
 }
-return b;
+char *fill_handle(f_args in) {
+  char a = in.fillchar ? in.fillchar : '\0';
+  int len = in.len ? in.len : 1;
+  char *s = in.strin ? in.strin : (char *)malloc((len + 1) * sizeof(char));
+  return fill_base(s, a, len);
 }
-char* fill_handle(f_args in){
-   char a=in.fillchar?in.fillchar:'\0';
-   int len=in.len?in.len:1;
-   char *s =in.strin ?in.strin:(char*)malloc((len+1)*sizeof(char));
-   return fill_base(s,a,len);
-}
-int main(){
-    int l;
-    char A;
-    scanf("%d %c",&l,&A);
-    char *s=fill(.fillchar=A,.len=l);
-    *(s+l)='\0';
-    for(char* i=s;*i!='\0';i++){
-        printf("%c",*i);
-    }
-    return 0;
-    free(s);
+int main() {
+  int l;
+  char A;
+  scanf_s("%d %c", &l, &A);
+  char *s = fill(.fillchar = A, .len = l);
+  *(s + l) = '\0';
+  for (char *i = s; *i != '\0'; i++) {
+    printf("%c", *i);
+  }
+  return 0;
+  free(s);
 }
