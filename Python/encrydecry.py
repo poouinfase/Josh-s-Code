60ae8cd4597e26c50fd95aa13b33db4ba6fbf9ed
Josh the cat <catterjeeo@gmail.com>
2024-01-20 00:00:32 +0530

    Worked more on python

--- Python/encrydecry.py
+++ Python/encrydecry.py
@@ -1,5 +1,7 @@
+
+
 class Encoder:
-    def __init__(self, key, val=16):
+    def __init__(self, key, val=8):
         self.siz = val
         self.key = key
 
@@ -13,19 +15,28 @@ class Encoder:
             | tupIn[1] << (2*siz) | tupIn[2] << (1*siz) | tupIn[3]
 
     def encrypt(self, base):
-        exp = self.key
+        exp = int(self.key)
         if (base == 0):
             return 0
         t = 1
         while (exp > 0):  # Simplifying multiplication by squaring itself
-            if ((exp & 1) == 1):
+            if ((exp & 1) != 0):
                 t = t * base
             base = base * base
             exp >>= 1
         return t
 
-    def decrypt(self, n):
-        return int(n**(1/self.key))
+    def decrypt(self, base):
+        return int(base**(1/self.key))
+
+    # def decrypt(self, base):
+    #     ex = self.key
+    #     while ex:
+    #         if (ex & 1):
+    #             base **= 1.
+    #         base **= 0.5
+    #         ex >>= 1
+    #     return int(base)
 
     def decode(self, n):
         siz = self.siz
@@ -46,9 +57,13 @@ def brk(strin, siz):
     return list(map("".join, lisBrok))
 
 
-if __name__ == "__main__":
+def main():
     lis = brk(input(), 4)
     Worker = Encoder(8)
     print(ren := [Worker.encrypt(Worker.encode(i)) for i in lis])
     print("".join([Worker.decode(Worker.decrypt(i)) for i in ren]))
+    quit()
 
+
+if __name__ == "__main__":
+    main()
