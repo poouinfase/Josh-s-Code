383c3fa9f498aa372107c9dab4323b486baf07ef
Josh the cat <catterjeeo@gmail.com>
2024-01-20 15:27:59 +0530

    Got the Ticks working

--- C/CompLinBIn/Comp.ps1
+++ C/CompLinBIn/Comp.ps1
@@ -1,17 +1,16 @@
 $Srch="C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\Srch.exe";
 $fil= "C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\fil.exe";
 if ((Test-Path -path $Srch ) -eq $False) { gcc BinVerLin.c -O2 -o Srch.exe; } #Complie if No executable found
-if ((Test-Path -path $fil ) -eq $False) { gcc filter.c -O2 -o fil.exe; } #Complie if No executable found
+# if ((Test-Path -path $fil ) -eq $False) { gcc filter.c -O2 -o fil.exe; } #Complie if No executable found
 echo ''>Timings.txt
 foreach ($algonum in '1','2') {
   # echo $algonum
-  $lim = [uint64] 1 -shl 43-1
+  $lim = ([uint64] 1 -shl 32)-1
   $Siz=[uint64] 1
   for ($Siz ; $Siz -lt ($lim); $Siz =$Siz -shl 1) {
     echo $Siz;
     $Tim=time{ $algonum|.\Srch.exe $Siz }
-    $D=.\fil.exe $Tim
-    # echo $algonum
+    $D=($Tim| Format-Table -Property Ticks -HideTableHeaders|Out-String).Trim()    # echo $algonum
     echo "$algonum $Siz $D" >>Timings.txt
      }
 }
--- C/CompLinBIn/Stats.py
+++ C/CompLinBIn/Stats.py
@@ -11,10 +11,9 @@ for i in stin:
     listLook = i.split()
     pi = parseDict[listLook[0]]
     pi['x'].append(int(listLook[1]))
-    pi['y'].append(float(listLook[2]))
-plt.plot(algo1['x'], algo1['y'])
-plt.plot(algo2['x'], algo2['y'])
+    pi['y'].append(int(listLook[2]))
+plt.plot(algo1['x'], algo1['y'], label='Binary')
+plt.plot(algo2['x'], algo2['y'], label='Linear')
 print(algo1['x'], algo1['y'])
+plt.savefig('Compgraph.png')
 plt.show()
-plt.savefig('Comp.png')
-
