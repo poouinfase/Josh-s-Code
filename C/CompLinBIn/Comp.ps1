2e53dfe272c3ab461f63b5ef1678f672c298c5a7
Josh the cat <catterjeeo@gmail.com>
2024-01-20 13:36:02 +0530

    FIlter and pwsh script set up

---
+++ C/CompLinBIn/Comp.ps1
@@ -0,0 +1,17 @@
+$Srch="C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\Srch.exe";
+$fil= "C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\fil.exe";
+if ((Test-Path -path $Srch ) -eq $False) { gcc BinVerLin.c -o Srch.exe; } #Complie if No executable found
+if ((Test-Path -path $fil ) -eq $False) { gcc filter.c -O2 -o fil.exe; } #Complie if No executable found
+echo ''>Timings.txt
+foreach ($algonum in '1','2') {
+  # echo $algonum
+  $lim = [uint64] 1 -shl 33-1
+  $Siz=[uint64] 1
+  for ($Siz ; $Siz -lt ($lim); $Siz =$Siz -shl 1) {
+    # echo $Siz;
+    $Tim=time{ $algonum|.\Srch.exe $Siz }
+    $D=.\fil.exe $Tim
+    # echo $algonum
+    echo "$algonum $Siz $D"
+  }
+}
