63ec468fbe8ca52507b57080867e25313f1cb04f
Josh the cat <catterjeeo@gmail.com>
2024-01-20 13:37:19 +0530

    Changed Comp to write to a file

--- C/CompLinBIn/Comp.ps1
+++ C/CompLinBIn/Comp.ps1
@@ -1,6 +1,6 @@
 $Srch="C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\Srch.exe";
 $fil= "C:\Users\catte\OneDrive\Documents\My\FUqaround\C\CompLinBIn\fil.exe";
-if ((Test-Path -path $Srch ) -eq $False) { gcc BinVerLin.c -o Srch.exe; } #Complie if No executable found
+if ((Test-Path -path $Srch ) -eq $False) { gcc BinVerLin.c -O2 -o Srch.exe; } #Complie if No executable found
 if ((Test-Path -path $fil ) -eq $False) { gcc filter.c -O2 -o fil.exe; } #Complie if No executable found
 echo ''>Timings.txt
 foreach ($algonum in '1','2') {
@@ -12,6 +12,6 @@ foreach ($algonum in '1','2') {
     $Tim=time{ $algonum|.\Srch.exe $Siz }
     $D=.\fil.exe $Tim
     # echo $algonum
-    echo "$algonum $Siz $D"
-  }
+    echo "$algonum $Siz $D" >>Timings.txt
+     }
 }
