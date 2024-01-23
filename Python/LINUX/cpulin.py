ab6bd1ee5e126abd12b1efac79372f7cf4aac207
Josh the cat <catterjeeo@gmail.com>
2024-01-23 21:32:21 +0530

    PYTHON/LINUX/cpulin.py

--- Python/LINUX/cpulin.py
+++ Python/LINUX/cpulin.py
@@ -82,6 +82,42 @@ def PRIORITY_PREMPTIVE():
     print(*proclis, sep='\n')
 
 
+def PRIORITY_NONPREMPTIVE():
+    time = 0
+    f = open('Proctabl.txt', 'r')
+    proclis = []
+    for procAt in f.readlines():
+        proclis.append(process(*procAt.split()))
+    f.close()
+    procomp = 0
+    ready = []
+    best = None
+    while procomp < len(proclis):
+        for i in proclis:
+            if i[4] == time:
+                ready.append(i)
+        if len(ready) == 0:
+            time += 1
+            continue
+        best = ready[0]
+        if best is None:
+            for i in ready:
+                if i[2] < best[2]:
+                    best = i
+                if i[2] == best[2]:
+                    best = best if (best[4] < i[4]) else i
+        for i in ready:
+            if best is not i:
+                i[3] += 1
+        best[1] += -1
+        if best[1] == 0:
+            ready.remove(best)
+            best = None
+            procomp += 1
+        time += 1
+    print(*proclis, sep='\n')
+
+
 def SJF_PREMPTIVE():
     time = 0
     f = open('Proctabl.txt', 'r')
@@ -115,8 +151,43 @@ def SJF_PREMPTIVE():
     print(*proclis, sep='\n')
 
 
-def ROUNDROBIN():
-    TimQ = 20
+def SJF_NONPREMPTIVE():
+    time = 0
+    f = open('Proctabl.txt', 'r')
+    proclis = []
+    for procAt in f.readlines():
+        proclis.append(process(*procAt.split()))
+    f.close()
+    procomp = 0
+    ready = []
+    best = None
+    while procomp < len(proclis):
+        for i in proclis:
+            if i[4] == time:
+                ready.append(i)
+        if len(ready) == 0:
+            time += 1
+            continue
+        best = ready[0]
+        if best is None:
+            for i in ready:
+                if i[1] < best[1]:
+                    best = i
+                if i[1] == best[1]:
+                    best = best if (best[4] < i[4]) else i
+        for i in ready:
+            if best is not i:
+                i[3] += 1
+        best[1] += -1
+        if best[1] == 0:
+            ready.remove(best)
+            procomp += 1
+            best = None
+        time += 1
+    print(*proclis, sep='\n')
+
+
+def ROUNDROBIN(TimQ=20):
     time = 0
     f = open('Proctabl.txt', 'r')
     proclis = []
@@ -149,8 +220,14 @@ def ROUNDROBIN():
     print(*proclis, sep='\n')
 
 
-# FCFS()
-# PRIORITY_PREMPTIVE()
-# SJF_PREMPTIVE()
-ROUNDROBIN()
+print("1 FCFS\n2 PRIORITIRY_PREMPTIVE\n3 PRIORITY_NONPREMEPTIVE",
+      "\n4 SJF_PREMPTIVE\n5 SJF_NONPREMPTIVE\n6 ROUNDROBIN", sep='')
 
+
+match(int(input())):
+    case 1: FCFS()
+    case 2: PRIORITY_PREMPTIVE()
+    case 3: PRIORITY_NONPREMPTIVE()
+    case 4: SJF_PREMPTIVE()
+    case 5: SJF_NONPREMPTIVE()
+    case 6: ROUNDROBIN()
