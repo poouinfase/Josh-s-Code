17c2a782b34d050fa70aa200fa1e4d0ea13caefc
Josh the cat <catterjeeo@gmail.com>
2024-01-24 14:35:00 +0530

    ADDED PaddedAsteriskPattern

---
+++ C/SoopClass/PaddedAsteriskPattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int roSiz = 0;
+  printf("ENTER ROWS: ");
+  scanf_s("%d", &roSiz);
+  for (int siz = 1; siz <= roSiz; siz++) {
+    for (int j = 0; j < roSiz - siz; j++)
+      putc(' ', stdout);
+    for (int j = 0; j < siz; j++) {
+      printf("*");
+    }
+    putc('\n', stdout);
+  }
+}
