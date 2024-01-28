6095ed9c0c1c83740ec08c89656e334a6f1e20c4
Josh the cat <catterjeeo@gmail.com>
2024-01-28 16:46:50 +0530

    Charref added
    Its a tool for charcater stuff

---
+++ C/TEXT_EDITOR/Charref.c
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int chrc = 0;
+  for (char i = _getch(); i != '\0'; i = _getch()) {
+    printf("%c %d\n", i, i);
+    chrc++;
+  }
+  printf("\n%d\n", chrc);
+  return EXIT_SUCCESS;
+}
