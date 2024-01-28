803f03c5fc71911c3cd827408f181cde9badcf5a
Josh the cat <catterjeeo@gmail.com>
2024-01-28 16:26:06 +0530

    Started work on Texteditor  Charref tool was made

---
+++ C/TEXT_EDITOR/TextEditor.c
@@ -0,0 +1,36 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+void keymove(char *i) {
+  *i = _getch();
+  putc(0x1B, stdout);
+  putc('[', stdout);
+  switch (*i) {
+  case 'H':
+    *i = 'A';
+    break;
+  case 'K':
+    *i = 'D';
+    break;
+  case 'P':
+    *i = 'B';
+    break;
+  case 'M':
+    *i = 'C';
+    break;
+  }
+}
+int main(int argc, char *argv[]) {
+  int chrc = 0;
+  for (char i = _getch(); i != '\0'; i = _getch()) {
+    if (i == 13)
+      i = '\n';
+    if (i == -32)
+      keymove(&i);
+
+    putc(i, stdout);
+    chrc++;
+  }
+  printf("\n%d\n", chrc);
+  return EXIT_SUCCESS;
+}
