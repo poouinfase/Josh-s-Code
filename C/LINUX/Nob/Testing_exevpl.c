27d242f1ea90411098204b0ec4525b48974924ff
Josh the cat <catterjeeo@gmail.com>
2024-02-02 15:01:19 +0530

    Linux exec

--- C/LINUX/Nob/Testing_exevpl.c
+++ C/LINUX/Nob/Testing_exevpl.c
@@ -1,13 +1,17 @@
-#include <unistd.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-int main(int argc, char * argv[]) {
-  char **arv;
-  arv=(char**)malloc(3*sizeof(char*));
-  // *(arv+1)="--rec";
-  *(arv+0)="../../..";
-  if(execvp("ls",arv)==-1)printf("ERROR\n %s",(char*)strerror(errno));
-  free(arv);
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+  char** const arv=calloc(6, sizeof(char));
+  arv[0]="gcc";
+  arv[1]="-g";
+  arv[2]="-o";
+  arv[2]="Bingus";
+  arv[2]="Testing_exevpl.c";
+  arv[3]="\0";
+
+  if (execvp("ls", arv) == -1)
+    printf("ERROR\n %s", (char *)strerror(errno));
   return EXIT_SUCCESS;
 }
