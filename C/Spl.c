8b16fe710876d402ca34a20230fa9bff27d30618
Josh the cat <catterjeeo@gmail.com>
2024-02-12 22:26:51 +0530

    Split finally actually works
    It resturns a char** now

--- C/Spl.c
+++ C/Spl.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define split(...) split_handle((split_args){__VA_ARGS__})
+
+typedef struct {
+  char *strin;
+  char sep;
+} split_args;
+
+typedef struct {
+  char **StrArray;
+  int len;
+} CstrView;
 
 char *stcp(char *strin, int strt, int end) {
-  char *out = "\0";
-  if (end < strt)
-    return out;
-
-  out = (char *)malloc(sizeof(char) * (end - strt + 1) + 1);
+  if (end < strt) {
+    return NULL;
+  }
+  char *out =
+      (char *)malloc(sizeof(char) * (end - strt + 1) + 1 * (sizeof(char)));
   char *poin = out;
   for (char *i = strin + strt; (strin + end) - i >= 0; i++) {
     *poin = *i;
@@ -16,25 +28,41 @@ char *stcp(char *strin, int strt, int end) {
   return out;
 }
 
-char **split(char *strin, char sep) {
-  char **out;
+CstrView split_worker(char *strin, char sep) {
+  char **out = malloc(sizeof(char *) * strlen(strin));
+  int wor = 0;
   int lef = 0;
   int rig = 0;
   for (char *i = strin; *i != '\0'; i++) {
     if (*(i) == sep || *(i + 1) == '\0') {
-      int h = (*i == sep) ? 1 : 0;
-      printf("%d %d\t%s\n", lef, rig - h, stcp(strin, lef, rig));
+      int h = (*i == sep);
+      char *s = stcp(strin, lef, rig - h);
+      if (s != NULL) {
+        // printf("%d %d\t%s\n", lef, rig - h, s);
+        out[wor++] = s;
+      }
       lef = rig;
       lef++;
     }
     rig++;
   }
-  return out;
+  out = realloc(out, wor * sizeof(char *));
+  return (CstrView){.StrArray = out, .len = wor};
+}
+CstrView split_handle(split_args In) {
+  In.sep = (In.sep) ? In.sep : ' ';
+  return split_worker(In.strin, In.sep);
 }
 
 int main() {
   char *strin = (char *)malloc(sizeof(char) * 50);
-  scanf_s("%[^\n]*s", strin);
-  // printf_s("%s\n", strin);
-  split(strin, ' ');
+  if (scanf("%[^\n]*s", strin) == 0)
+    return -1;
+  const CstrView out = split(strin);
+  free(strin);
+  for (int i = 0; i < out.len; i++) {
+    printf("%s\n", out.StrArray[i]);
+    free(out.StrArray[i]);
+  }
+  free(out.StrArray);
 }
