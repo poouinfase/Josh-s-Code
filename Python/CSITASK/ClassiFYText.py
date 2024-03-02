543c5c5066d827d097fec3072d5cd73e01102519
Josh the cat <catterjeeo@gmail.com>
2024-03-02 22:32:56 +0530

    CSITASKFINAL

--- Python/CSITASK/ClassiFYText.py
+++ Python/CSITASK/ClassiFYText.py
@@ -1,4 +1,4 @@
-import zlib
+import gzip as zlib
 
 def CompLen(st:str):
     return len(zlib.compress(st.encode()))
@@ -7,13 +7,12 @@ def NormalizeCompressDistance(stX:str,stY:str):
     LeXY:int=CompLen(stX+stY)
     LenX:int=CompLen(stX)
     LenY:int=CompLen(stY)
-    NCD:float = LeXY-min(LenX,LenY)
-    NCD /= max(LenX,LenY)
+    NCD:float = (LeXY-min(LenX,LenY))/max(LenX,LenY)
     return NCD
 
 def main():
-    BASE = "food poisoning chicken Tarama 17th febuary"
-    Test =[BASE,"We are here about your car's extended warrantee", "Yeah, I got food poisoning a on the 17th of febuary. I had the chicken tikka at Tarama", "The hot singles have breached containment in your area, evacuate immediately","I know where you live"]
+    BASE = "food poisoning chicken Tarama 17th february"
+    Test =[BASE,"We are here about your car's extended warranty", "Yeah, I got food poisoning on the 17th of february. I had the chicken tikka at Tarama", "The hot singles have breached containment in your area, evacuate immediately","I know where you live"]
     coun =1;
     for i in Test:
         print(coun,NormalizeCompressDistance(BASE.lower(),i.lower()),i)
