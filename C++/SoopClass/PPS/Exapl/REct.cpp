3c1846ec483bfd866c57bd3e56dc278f5e26187a
Josh the cat <catterjeeo@gmail.com>
2024-04-03 15:20:53 +0530

    Practice For SOOp

--- C++/SoopClass/PPS/Exapl/REct.cpp
+++ C++/SoopClass/PPS/Exapl/REct.cpp
@@ -1,3 +1,5 @@
+#ifdef DEBUG
+
 #include <iostream>
 using namespace std;
 class Rectangle {
@@ -39,3 +41,45 @@ int main() {
   aa.out();
   return 0;
 }
+#else
+#include <iostream>
+
+using namespace std;
+class Rectangle {
+  int length, width;
+
+public:
+  int Area, Perimeter;
+  void getdata() {
+    cout << "length =";
+    cin >> length;
+    cout << " width = ";
+    cin >> width;
+  }
+  float getArea() {
+    if (length > 0 && width > 0) {
+      Area = length * width;
+      return Area;
+    } else {
+      cout << "Area = Invalid" << endl;
+      return -1;
+    }
+  }
+  float getPerimeter() {
+    if (length > 0 && width > 0) {
+      Perimeter = 2 * (length + width);
+      return Perimeter;
+    } else {
+      cout << "Perimeter = Invalid" << endl;
+      return -1;
+    }
+  }
+};
+int main() {
+  Rectangle aa;
+  aa.getdata();
+  cout << "Area is " << aa.getArea() << endl;
+  cout << "Perimeter is " << aa.getPerimeter();
+  return 0;
+}
+#endif // DEBUG
