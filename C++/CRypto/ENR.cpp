422c17214395738daa8a517a83d4f8f6e2a6135b
Josh the cat <catterjeeo@gmail.com>
2024-04-06 00:11:02 +0530

    Switch statmene added

--- C++/CRypto/ENR.cpp
+++ C++/CRypto/ENR.cpp
@@ -433,9 +433,23 @@ public:
 };
 
 int main() {
+  std::cout << "CHOICES:\n1.SHA-512\n2.MD5" << std::endl;
+
+  int n = 0;
+  std::cin >> n;
+
+  std::cout << "Message: ";
+
   std::string Message;
   std::cin >> Message;
+  std::cout << std::endl;
+  switch (n) {
 
-  Sha512(Message).out();
-  MD5(Message).out();
+  case 1:
+    Sha512(Message).out();
+    break;
+  case 2:
+    MD5(Message).out();
+    break;
+  }
 }
